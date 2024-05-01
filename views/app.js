c600e1cd88c3b5f33a7af015ae3840443320df6a
Bruna Baria <bruna.baria.m@gmail.com>
2024-05-01 11:16:39 -0300

    page flow

---
+++ views/app.js
@@ -0,0 +1,11 @@
+//verificar funcionalidade do botão voltar
+function voltarParaPaginaInicial() {
+    window.location.href = "index.html";
+}
+function irParaReservas(){
+    window.location.href = "reserva.html";
+}
+//Implementar callback para 
+//1° - Quando selecionar um local -> mostre a imagem do local
+//2° - neste local puxar os tipos de espaços disponíveis
+//3° - Ao selecionar o tipo de local abra um calendário interativo com as datas já reservadas
