b0dc65c1f41d00369f0e8cee5f9b8a2848a2a2c0
FIAPflix <156673549+FIAPflix@users.noreply.github.com>
2024-05-04 16:01:45 -0300

    Ler reservas do supabase
    Adicionado ler tabela do supabase através da rota /agendamentos

--- routes/home.js
+++ routes/home.js
@@ -7,26 +7,40 @@ dotenv.config()
 const supabaseClient = supabase.createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 router.get('/', (req, res) => {
-    res.render('index');
+  res.render('index');
 });
 
-router.get('/reserva',(req, res) => {
-    res.render('reserva');
+router.get('/reserva', (req, res) => {
+  res.render('reserva');
 })
 
+router.get('/agendamentos', async (req, res) => {
+  try {
+    // console.log(req.body);
+    const jsonData = req.body;
+    const { data, error } = await supabaseClient.from("db_Reservas").select('*')
+    if (error) {
+      console.log(`Error fetching data from Supabase: ${error}`);
+    }
+    res.render('agendamentos', {data:data})
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 router.post('/enviar-reserva', async (req, res) => {
-    try {
-        // console.log(req.body);
-        const jsonData = req.body;
-        const { data, error } = await supabaseClient.from("db_Reservas").insert(jsonData);
-        if (error) {
-          return res.status(400).json(error);
-        }
-        // res.status(200).json(req.body);
-        // Adicionar aqui a resposta do agendamento (ex. alert com agendamento feito)
-      } catch (error) {
-        res.status(500).json({ error: "Internal server error" });
-      }
-    });
+  try {
+    // console.log(req.body);
+    const jsonData = req.body;
+    const { data, error } = await supabaseClient.from("db_Reservas").insert(jsonData);
+    if (error) {
+      return res.status(400).json(error);
+    }
+    // res.status(200).json(req.body);
+    // Adicionar aqui a resposta do agendamento (ex. alert com agendamento feito)
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 module.exports = router;
